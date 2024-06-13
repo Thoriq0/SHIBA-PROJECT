1b7e0bf1146eb5be0fe0c7ddb65727754986f92e
ThoriqLX <thoriq.ahmad1301@gmail.com>
2024-06-13 18:31:17 +0700

    configure shiba news

--- src/scripts/dataProcessing.js
+++ src/scripts/dataProcessing.js
@@ -1,9 +1,11 @@
-import {getAllParamShiba, getDailyShiba, getListShiba, getFiveMinuteShiba, getDailyBmkg} from './api'
+import { getAllParamShiba, getDailyShiba, getDailyBmkg, shibaNews } from './api';
 
 async function dailyProcess(){
   try{
     const bmkg = await getDailyBmkg();
     const shiba = await getDailyShiba();
+    // console.log(bmkg);
+    // console.log(shiba);
     return {bmkg, shiba};
   }
   catch(error){
@@ -11,90 +13,233 @@ async function dailyProcess(){
   }
 }
 
-async function deleteAllData(url) {
+async function deleteFilteredData(url, data) {
   try {
+    const dataLength = data.length;
+    const dataToKeep = data.slice(-4); // Ambil 4 objek terakhir dari array data
+
+    if (dataLength <= 4) {
+      console.log('No need to delete, keeping all data');
+      return;
+    }
+
     const response = await fetch(url, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
+      body: JSON.stringify(dataToKeep)
     });
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    console.log('All data successfully deleted');
+
+    console.log('Data successfully deleted, keeping the last 4 items:', dataToKeep);
   } catch (error) {
-    console.log('Error:', error);
+    console.log('Error in deleteFilteredData:', error);
   }
 }
 
-export default async function processDaily(){
+export async function processDaily() {
+  const urlShiba = process.env.API_DAILYSHIBA;
 
-  const urlShiba = process.env.API_DAILYSHIBA
+  try {
+    const { bmkg, shiba } = await dailyProcess();
 
-  const {bmkg, shiba} = await dailyProcess();
-
-  // console.log(bmkg.Infogempa)
-  // console.log(shiba);
-
-  if (!bmkg || !shiba) {
-    console.log('Failed to fetch data from BMKG or Shiba API');
-    return;
-  }
-
-  const bmkgDateTime = bmkg.Infogempa.gempa.DateTime;
-  const bmkgDate = new Date(bmkgDateTime);
-
-  // Convert shiba data from object to array
-  const shibaArray = Object.values(shiba);
-
-  // Check if the month has changed
-  const currentMonth = bmkgDate.getMonth();
-  const currentYear = bmkgDate.getFullYear();
-  const dataMonthChanged = shibaArray.some(item => {
-    const itemDate = new Date(item.DateTime);
-    return itemDate.getMonth() !== currentMonth || itemDate.getFullYear() !== currentYear;
-  });
-
-  if (dataMonthChanged) {
-    console.log('Month has changed, deleting all data');
-    await deleteAllData(urlShiba);
-  }
-
-  // Check if the DateTime from BMKG exists in Shiba data
-  const exists = shibaArray.some(item => item.DateTime === bmkgDateTime);
-
-  if (!exists) {
-    console.log('No matching DateTime found in Shiba data, pushing data to API');
-
-    // Push data to Shiba API
-    async function pushDaily(bmkgData) {
-      try {
-        const response = await fetch(urlShiba, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(bmkgData)
-        });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        console.log('Data Berhasil Dikirim');
-      } catch (error) {
-        console.log('Error:', error);
-      }
+    if (!bmkg || !shiba) {
+      console.log('Failed to fetch data from BMKG or Shiba API');
+      return;
     }
 
-    await pushDaily(bmkg.Infogempa.gempa);
-  } else {
-    console.log('Matching DateTime found in Shiba data, not pushing data');
+    const bmkgDateTime = bmkg.Infogempa.gempa.DateTime;
+    const bmkgDate = new Date(bmkgDateTime);
+
+    const shibaArray = Object.values(shiba);
+    if (shibaArray.length === 0) {
+      console.log('Shiba data array is empty.');
+      return;
+    }
+
+    const currentMonth = bmkgDate.getMonth();
+    const currentYear = bmkgDate.getFullYear();
+    const dataMonthChanged = shibaArray.some(item => {
+      const itemDate = new Date(item.DateTime);
+      return itemDate.getMonth() !== currentMonth || itemDate.getFullYear() !== currentYear;
+    });
+
+    if (dataMonthChanged) {
+      console.log('Month has changed, filtering and deleting data if necessary');
+      await deleteFilteredData(urlShiba, shibaArray);
+    }
+
+    const exists = shibaArray.some(item => item.DateTime === bmkgDateTime);
+
+    if (!exists) {
+      console.log('No matching DateTime found in Shiba data, pushing data to API');
+
+      async function pushDaily(bmkgData) {
+        try {
+          const response = await fetch(urlShiba, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bmkgData)
+          });
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          console.log('Data Berhasil Dikirim');
+        } catch (error) {
+          console.log('Error in pushDaily:', error);
+        }
+      }
+
+      await pushDaily(bmkg.Infogempa.gempa);
+    } else {
+      console.log('Matching DateTime found in Shiba data, not pushing data');
+    }
+  } catch (error) {
+    console.log('Error in processDaily:', error);
   }
-
-
 }
 
+// NEWS
 
+// FOR TESTING FAKE DATE
+// class DateWrapper {
+//   constructor() {
+//     this._currentDate = new Date();
+//   }
+
+//   // Method untuk mendapatkan waktu saat ini
+//   now() {
+//     return this._currentDate;
+//   }
+
+//   // Method untuk mengatur waktu
+//   setCurrentDate(date) {
+//     this._currentDate = new Date(date);
+//   }
+// }
+
+
+async function getNews() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.NEWS_KEY,
+      'x-rapidapi-host': 'google-news13.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await fetch(process.env.API_NEWS, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    const news = responseData.items;
+
+    if (!Array.isArray(news)) {
+      console.log('Received data is not an array:', news);
+      throw new Error('Received data is not an array');
+    }
+
+    return news;
+  } catch (error) {
+    console.log('Error in getNews:', error.message);
+    throw error;
+  }
+}
+
+async function deleteNonTesData(url, data) {
+  try {
+    const filteredData = data.filter(item => item.tes === "");
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(filteredData)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    console.log('All non-tes data successfully deleted');
+  } catch (error) {
+    console.log('Error in deleteNonTesData:', error);
+    throw error;
+  }
+}
+
+let lastPushTime = null;
+
+export async function pushNews() {
+
+  // TESTING FAKE DATE
+  // const dateWrapper = new DateWrapper();
+  // function getCurrentTime() {
+  //   return dateWrapper.now();
+  // }
+  // dateWrapper.setCurrentDate('2024-06-14T07:00:00Z'); 
+  // console.log(getCurrentTime());
+
+  const currentDate = new Date();
+  const dayOfMonth = currentDate.getDate();
+  const currentHour = currentDate.getHours();
+  const currentMinute = currentDate.getMinutes();
+  const currentSecond = currentDate.getSeconds();
+
+  
+  
+  if (dayOfMonth % 2 === 0 && currentHour >= 7 && currentMinute === 0 && currentSecond === 0) {
+    try {
+      if (lastPushTime === dayOfMonth) {
+        console.log('Data telah diperbaharui pada pukul 7 hari ini.');
+        return;
+      }
+
+      const news = await getNews();
+
+      if (!Array.isArray(news)) {
+        console.log('Received data is not an array:', news);
+        throw new Error('Received data is not an array');
+      }
+
+      await deleteNonTesData(process.env.API_NEWSHIBA, news);
+
+      const response = await fetch(process.env.API_NEWSHIBA, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(news)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      console.log('Response from API:', responseData); 
+
+      // Update lastPushTime ke hari ini
+      lastPushTime = dayOfMonth;
+
+    } catch (error) {
+      console.log('Error in pushNews:', error);
+    }
+  } else {
+    console.log('Hari ini tanggal ganjil atau belum pukul 7, tidak ada pengiriman berita.');
+    // console.log(new DateWrapper().now())
+  }
+}
 
 
 
--- src/scripts/leaflet.js
+++ src/scripts/leaflet.js
@@ -1,6 +1,6 @@
 import L from 'leaflet';
 
-
+// INDEX MAP GET ONE
 export function currentInitializeMap() {
   const map = L.map('map').setView([-6.200000, 106.816666], 5); // Lokasi awal di Jakarta
 
@@ -35,4 +35,31 @@ export function currentAddMarkerToMap(map, lat, lon) {
 
   // Pusatkan peta di koordinat gempa
   map.setView([lat, lon], 13);
+}
+
+
+// GET MANY MAPS
+export function fiveMInitializeMap(index){
+  const map = L.map(`map${index}`).setView([-6.200000,106.816666], 5);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 30
+  }).addTo(map);
+
+  return map;
+}
+
+export function fiveMAddMarkerToMap(map, lat, lon){
+  const myIcon = L.icon({
+    iconUrl: 'https://firebasestorage.googleapis.com/v0/b/shiba-capstone-94834.appspot.com/o/warning.png?alt=media&token=3a73e1f3-819d-4aa1-9282-5c266a67a926',
+    iconSize: [40, 40],
+    iconAnchor: [32, 54],
+    popupAnchor: [0, -54]
+  });
+
+  L.marker([lat, lon], {icon: myIcon}).addTo(map)
+    .bindPopup(`<b>Koordinat Gempa:</b><br>Latitude: ${lat}<br>Longitude: ${lon}`)
+    .openPopup();
+
+  map.setView([lat, lon], 10);
 }
