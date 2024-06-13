6ac4c7edbd35bc42e936cd8fc7f30787db7ac219
ThoriqLX <thoriq.ahmad1301@gmail.com>
2024-06-13 18:31:17 +0700

    configure shiba news

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,6 +1,6 @@
 // API ENDPOINT BMKG
 const bmkgDaily = process.env.API_DALYBMKG;
-const bmkgList = process.env.API_LISTBMKG;
+// const bmkgList = process.env.API_LISTBMKG;
 const bmkgFiveM = process.env.API_MFIVEBMKG;
 
 // API SHIBA 
@@ -8,6 +8,10 @@ const allSource = process.env.API_ALLPARAMSHIBA;
 const shibaDaily = process.env.API_DAILYSHIBA;
 const shibaList = process.env.API_LISTSHIBA;
 const shibaFiveM = process.env.API_MFIVESHIBA;
+
+// NEWS SHIBA
+const news = process.env.API_NEWS;
+const newShiba = process.env.API_NEWSHIBA;
 
 export async function getAllParamShiba(){
   try{
@@ -54,6 +58,9 @@ export async function getFiveMinuteShiba(){
 }
 
 
+
+// BMKG
+
 export async function getDailyBmkg(){
   try{
     const response = await fetch(bmkgDaily);
@@ -65,6 +72,47 @@ export async function getDailyBmkg(){
   }
 }
 
+export async function getFiveMBmkg(){
+  try{
+    const response = await fetch(bmkgFiveM);
+    const data = await response.json();
+    return data;
+  }
+  catch(error){
+    console.log('Something Wrong ' + error.message);
+  }
+}
+
+
+// NEWS
+// export async function getNews(){
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       'x-rapidapi-key': process.env.NEWS_KEY,
+//       'x-rapidapi-host': 'google-news13.p.rapidapi.com'
+//     }
+//   }
+  
+//   try{
+//     const response = await fetch(news, options);
+//     const data = await response.json();
+//     return data;
+//   } catch(error) {
+//     console.log('Something Wrong ' + error.message);
+//   }
+// }
+
+export async function shibaNews(){
+  try{
+    const response = await fetch(newShiba);
+    const data = await response.json();
+    return data;
+  }
+  catch(error){
+    console.log('Something Wrong ' + error.message);
+  }
+}
 
 // SHOWNG FOR DATA
 export async function getEarthquakeData() { 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,15 +3,15 @@ import "../styles/style.css";
 import 'flowbite';
 import 'flowbite/dist/flowbite.min.js';
 import 'leaflet';
-import { getEarthquakeData, getDailyBmkg, getDailyShiba} from './api.js';
-import { currentInitializeMap, currentAddMarkerToMap } from './leaflet.js';
-import processDaily from './dataProcessing.js';
-
+import { getEarthquakeData, getDailyBmkg, getDailyShiba, getFiveMBmkg, shibaNews} from './api.js';
+import { currentInitializeMap, currentAddMarkerToMap, fiveMInitializeMap, fiveMAddMarkerToMap } from './leaflet.js';
+import {processDaily, pushNews} from './dataProcessing.js';
 
 // HOMEPAGE
 const path = window.location.pathname;
 if(path == '/'){
   processDaily();
+  pushNews();
 
   // DAILY SHOW 
   getDailyBmkg().then(data => {
@@ -52,7 +52,7 @@ if(path == '/'){
   // Get Data For Showing to index
   getDailyShiba().then(data => {
     // Convert Shiba Daily To Array
-    let shibaArray = Object.values(data);
+    let shibaArray = Object.values(data).reverse();
 
     let cardCount = 0;
 
@@ -62,7 +62,7 @@ if(path == '/'){
     const currentMonthName = monthNames[currentDate.getMonth()];
 
     const getTitle = document.querySelector('.month');
-    getTitle.innerHTML = "("+currentMonthName+")";
+    getTitle.innerHTML = currentMonthName;
 
     shibaArray.forEach(item => {
       let city = [{ Wilayah: item.Wilayah }];
@@ -103,8 +103,104 @@ if(path == '/'){
 
     });
 
-      console.log(shibaArray);
+      // console.log(shibaArray);
   });
+
+  getFiveMBmkg().then(data => {
+
+    let fiveM = data.Infogempa.gempa;
+
+    let getFiveMContainer = document.querySelector('.fiveM');
+
+    let countFive = 0;
+
+    fiveM.forEach((earth, index) => {
+
+      let city = [{ Wilayah: earth.Wilayah }];
+
+      function getCityName(city) {
+        const word = city.split(" ");
+        let cityName = word[word.length - 1];
+        if (cityName.includes("-")) {
+            cityName = cityName.replace("-", " ");
+        }
+        return cityName;
+      }
+
+      let cityName = city.map(earthQuake => getCityName(earthQuake.Wilayah));
+
+      if(countFive < 4){
+        let itemFive = `
+
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row lg:max-w-screen-md hover:bg-gray-100">
+              <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="./images/p${countFive}.png" alt="Image">
+
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-shiba">${cityName}</h5>
+                  <p class="mb-3 font-normal text-black">${earth.Tanggal} pukul ${earth.Jam}, Terjadi gempa bumi
+                  di wilayah ${earth.Wilayah}. Dengan kekuatan Magnitudo ${earth.Magnitude} dan status ${earth.Potensi}</p>
+              </div>
+        </div>
+
+          `;
+          getFiveMContainer.innerHTML += itemFive;
+          countFive++;
+        }
+      });
+  });
+
+
+  // FOR NEWS
+
+  shibaNews().then(data => {
+
+    const firstValue = Object.values(data)[0];
+    let getNewsContainer = document.querySelector('.news-shiba');
+
+    let countNews = 0;
+
+    firstValue.forEach((items, index) =>{
+
+      if(countNews < 3){
+
+        let timestamp = items.timestamp;
+        let convert = parseInt(timestamp);
+        let date = new Date(convert);
+
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+
+        let dayOfWeek = date.getDay();
+
+        let daysOfWeek = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+        let dayName = daysOfWeek[dayOfWeek];
+
+        let formattedDate = `${dayName}, ${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+
+        let itemNews = `
+          <div class="max-w-full bg-white rounded-xl shadow-md shadow-gray-500">
+              <img class="w-full h-auto rounded-t-lg" src="${items.images.thumbnailProxied}" alt="Image">
+              <div class="p-5">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-shiba">${items.title}
+                      Gempa Bumi</h5>
+                  <p class="mb-3 font-semibold text-black">${items.snippet}</p>
+                  <a href="${items.newsUrl}" class="inline-flex items-center text-blue-600 hover:underline" target="_blank">
+                      Baca Selengkapnya >>
+                  </a>
+                  <div class="border-t-2 border-t-gray-400 mt-4">
+                      <p class="mt-3 text-sm text-gray-500">${formattedDate}</p>
+                  </div>
+              </div>
+          </div>
+        `
+        getNewsContainer.innerHTML += itemNews
+        countNews++;
+      }
+
+    })
+
+  })
 
 
 }
